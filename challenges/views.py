02ac7196ce71d72f3747b97af52f2c5d19e7267a
Pizanias <perolocooo@hotmail.com>
2022-07-20 17:46:50 +0300

    main commit

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month"
+    elif month == "february":
+        challenge_text = "Walk for at least 20 minutes"
+    elif month == "march":
+        challenge_text = "Learn Django for 60 minutes"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
