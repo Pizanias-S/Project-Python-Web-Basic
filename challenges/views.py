64fe16189444e2541d63b0e0dddcfa5cb90b8754
Pizanias <perolocooo@hotmail.com>
2022-07-20 20:20:31 +0300

    main commit

--- challenges/views.py
+++ challenges/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
-# Create your views here.
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month",
@@ -19,6 +18,19 @@ monthly_challenges = {
     "december": "Learn Django for 60 minutes"
 }
 
+# Create your views here.
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("months-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -33,6 +45,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
