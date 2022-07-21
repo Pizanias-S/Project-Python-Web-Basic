d22c3b61bc835ecad77d3fdeeac03f654f4cf908
Pizanias <perolocooo@hotmail.com>
2022-07-21 13:16:45 +0300

    main commit

--- python_django_monthly_challenges/settings.py
+++ python_django_monthly_challenges/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'python_django_monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / "challenges"/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
