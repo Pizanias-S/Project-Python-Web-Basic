9ebbe3f587acb2b0da04bfbcafe523c2516fa500
Pizanias <perolocooo@hotmail.com>
2022-07-21 17:52:18 +0300

    main commit

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+
 
 
 monthly_challenges = {
@@ -47,4 +48,4 @@ def monthly_challenge(request, month):
         })
 
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
